0ee3bb6b5533299557f194792f02afce303da2c0
MadzMadzik <mag.sobieszuk@gmail.com>
2017-05-03 22:37:13 +0200

    make changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-var a = prompt('podaj długość podstawy trójkąta');
-	h = prompt('podaj wysokość trójkąta');
+var a = prompt('podaj długość podstawy trójkąta'),
+	h = prompt('podaj wysokość trójkąta'),
 	triangleArea = a*h/2;
 
 console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
