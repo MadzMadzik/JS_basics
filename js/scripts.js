a2792ceb990d693f542f65b2f000f999d92ad6aa
Madzik <mag.sobieszuk@gmail.com>
2017-05-07 20:08:14 +0200

    Zadanie 9_6

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,18 @@
-var name = prompt('Wpisz swoje imię');
-alert('Witaj, ' + name);
-console.log('Witaj, ' + name);
+var a = 5,
+    h = 7;
+ 
+function getTriangleArea(a, h) {
+                if (a <= 0 || h <= 0) {
+      console.log('Nieprawidłowe dane');
+    } else {
+      return a*h/2;
+    }
+}
+ 
+console.log(getTriangleArea(10,6));
+ 
+var triangleArea1 = getTriangleArea(4, 3);
+var triangleArea2 = getTriangleArea(0, 3);
+var triangleArea3 = getTriangleArea(4, 9);
+ 
+console.log(triangleArea1, triangleArea2, triangleArea3);
