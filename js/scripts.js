e24e455a1a12f715da76bec4cce54c02607f6cd6
Madzik <mag.sobieszuk@gmail.com>
2017-05-07 20:56:04 +0200

    Zadanie 9.2

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,9 @@
-var a = 5,
-    h = 7;
- 
-function getTriangleArea(a, h) {
-                if (a <= 0 || h <= 0) {
-      console.log('Nieprawidłowe dane');
-    } else {
-      return a*h/2;
-    }
-}
- 
-console.log(getTriangleArea(10,6));
- 
-var triangleArea1 = getTriangleArea(4, 3);
-var triangleArea2 = getTriangleArea(0, 3);
-var triangleArea3 = getTriangleArea(4, 9);
- 
-console.log(triangleArea1, triangleArea2, triangleArea3);
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+var allNames = femaleNames.concat(maleNames);
+var newName = 'Marian';
+
+if (allNames.indexOf(newName) === -1) {
+	var x = allNames.push(newName);
+};
+console.log(allNames);
